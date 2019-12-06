32bc5870da6b1372096828cd84dc6d56d6fc7d26
Fabio Girlando <fabiogirlando@Fabios-MacBook-Pro.local>
2019-12-06 22:16:02 +0100

    Settings menu
    Graphics
    Fullscreen
    Resolution

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,11 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-
     public AudioMixer audioMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+
+    public TMP_Dropdown qualityDropdown;
+
+    Resolution[] resolutions;
+
+    void Start() 
+    {
+
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
@@ -19,6 +55,6 @@ public class SettingsMenu : MonoBehaviour
 
     public void SetFullScreen (bool isFullscreen)
     {
-        
+        Screen.fullScreen = isFullscreen;
     }
 }
