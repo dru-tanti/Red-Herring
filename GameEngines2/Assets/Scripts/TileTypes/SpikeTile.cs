5147fdd6bbb86fbf61156201c5f568d6d1fb049c
Andrew Tanti <dru.tanti@gmail.com>
2019-12-11 12:40:15 +0100

    Testing

---
+++ GameEngines2/Assets/Scripts/TileTypes/SpikeTile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Game/Tiles/Spike")]
+public class SpikeTile : Tile
+{
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Haqq!");
+        }
+    }
+}
