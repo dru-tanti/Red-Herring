1c31bfb2da327f2deb67a504eee5b968a3bd03d2
Michela <micmif@gmail.com>
2020-01-28 12:46:20 +0100

    Enemies
    Uploading primary enemy mechanics; some bug fixes need to be done

---
+++ GameEngines2/Assets/Scripts/Enemies/EnemyPatrol.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+  public float speed;
+  public float distance;
+
+  private bool movingRight = true;
+
+  public Transform groundDetection;
+
+ // Makes Enemy Patrol: If the groundDection does not detect ground, the enemy will flip and move the other way
+  void FixedUpdate()
+  {
+    transform.Translate(Vector2.right * speed * Time.deltaTime);
+    RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+     if(groundInfo.collider == false){
+         if(movingRight == true){
+             transform.eulerAngles = new Vector3(0, -180, 0);
+             movingRight = false;
+         } else {
+             transform.eulerAngles = new Vector3(0, 0, 0);
+             movingRight = true;
+         }
+        }
+ }
+  
+// If enemy collides with player, it deals damage (Debug Log is displayed for now)
+ void OnTriggerEnter2D(Collider2D collider)
+ {
+     if(collider.tag == "Player")
+     {
+         Debug.Log("Dealing Damage");
+     }
+ }
+}
---
+++ GameEngines2/Assets/Scripts/Enemies/EnemyShooting.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    private bool isAttacking = false;
+    public float speed;
+    public float time;
+    public float repeat;
+    private bool moveRight = true;
+    public Transform patrolStart;
+    public Transform patrolEnd;
+    private Vector2 target;
+    public Transform projectileSpawn;
+    public GameObject Projectile;
+
+    private void Start()
+    {
+        target = new Vector2(patrolEnd.position.x, patrolEnd.position.y);
+    }
+    void Update()
+    {
+        // If enemy is not in attack mode, it will continue to patrol
+        if(!isAttacking)
+        {
+            Patrol();
+        }
+    }
+// Patrolling with points set in Unity
+    void Patrol()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            if(moveRight)
+            {   
+                target = new Vector2(patrolStart.position.x, patrolStart.position.y);
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+                moveRight = false;
+            } else {                   
+                target = new Vector2(patrolEnd.position.x, patrolEnd.position.y);
+                transform.localScale = new Vector3(1f, 1f, 1f);
+                moveRight = true;
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collider) 
+    {
+        if(collider.tag == "Player")
+        {
+            isAttacking = true;
+            StartCoroutine(Shooting());
+        }
+    }
+
+    public IEnumerator Shooting() {
+        while(isAttacking) {
+            Instantiate(Projectile, projectileSpawn.position, projectileSpawn.rotation);
+            yield return new WaitForSeconds(this.time);    
+        }
+    }
+    // Method will instantiate the projectile prefab
+    void Shoot()
+    {
+
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        if(other.tag == "Player")
+        {
+            isAttacking = false;
+        }    
+    }
+}
